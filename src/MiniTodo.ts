0bc54795e69a0f3d6923e463c567db39b72a9b2f
luciano.ramello <luciano.ramello@vass.es>
2021-04-08 16:46:46 +0200

    add new methods

--- src/MiniTodo.ts
+++ src/MiniTodo.ts
@@ -14,11 +14,23 @@ export class MiniTodo extends LitElement {
   <p>hello ${this.name? this.name : "World"}</p>
   <label>Nombre</label>
   <input type="text" placeholder="Nombre por favor" value=${this.name} @input=${this.updateName}>
+
+    <button @click=${this.addName} >ADD</button>
+
+  <h2> people</h2>
+  <ul>
+    ${this.people.map(p=> html`
+    <li>${p.name}</li>`)}
+  </ul>
     `;
   }
   updateName(e: {target: HTMLInputElement}){
     this.name = e.target.value;
   }
 
+  addName(){
+    this.people = [... this.people,{name:this.name}];
+    this.name = " ";
+  }
   
 }
